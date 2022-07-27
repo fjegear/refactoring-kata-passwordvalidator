7b9ec353448a9a3ef3bcdd44a4f1d2ddc7904057
Francisco Javier Egea <fjaviegea@yahoo.es>
2022-07-27 22:59:04 +0200

    Add rest of validations

--- src/PasswordValidatorKata.Tests/PasswordValidatorTests.cs
+++ src/PasswordValidatorKata.Tests/PasswordValidatorTests.cs
@@ -19,6 +19,10 @@ namespace PasswordValidatorKata.Tests
         [Theory]
         [InlineData("userp@ssw0rd", "Password should have at least one uppercase character")]
         [InlineData("USERP@SSW0RD", "Password should have at least one lowercase character")]
+        [InlineData("UserP@ssword", "Password should have at least one digit")]
+        [InlineData("UserPassw0rd", "Password should have at least one special character")]
+        [InlineData("UserP@sw0rd", "Password should be at least 12 characters long")]
+        [InlineData("User   P@ss w0rd", "Password should not have any whitespaces")]
         public void Should_not_validate_When(string password, string message)
         {
             //Arrange
--- src/PasswordValidatorKata/PasswordValidator.cs
+++ src/PasswordValidatorKata/PasswordValidator.cs
@@ -13,13 +13,15 @@ namespace PasswordValidatorKata
         {
             new RegexValidationRule("[A-Z]", "Password should have at least one uppercase character"),
             new RegexValidationRule("[a-z]", "Password should have at least one lowercase character"),
+            new RegexValidationRule("[0-9]", "Password should have at least one digit"),
+            new RegexValidationRule("[!”#\\$%&'\\(\\)\\*\\+,-\\.\\:;<=>\\?@\\[/\\]^_`\\{\\|\\}~]", "Password should have at least one special character"),
+            new RegexValidationRule(".{12,}", "Password should be at least 12 characters long"),
+            new RegexValidationRule("\\s+$", "Password should not have any whitespaces"),
         };
 
         public ValidationResult Validate(ValidationData validationData)
         {
-            var messages = new List<string>();
-
-            messages = rules
+            var messages = rules
                 .Where(rule => !rule.Validate(validationData.Password))
                 .Select(rule => rule.Message).ToList();
 
