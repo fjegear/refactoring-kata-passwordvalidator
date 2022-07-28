c9f66d77ce0b957764ed35d60f3af38fcc75f1ee
Francisco Javier Egea <fjaviegea@yahoo.es>
2022-07-28 19:32:13 +0200

    Add must not have white space rule

--- src/PasswordValidatorKata.Tests/PasswordValidatorTests.cs
+++ src/PasswordValidatorKata.Tests/PasswordValidatorTests.cs
@@ -17,23 +17,25 @@ namespace PasswordValidatorKata.Tests
         }
 
         [Theory]
-        [InlineData("userp@ssw0rd", "Password should have at least one uppercase character")]
-        [InlineData("USERP@SSW0RD", "Password should have at least one lowercase character")]
-        [InlineData("UserP@ssword", "Password should have at least one digit")]
-        [InlineData("UserPassw0rd", "Password should have at least one special character")]
-        [InlineData("UserP@sw0rd", "Password should be at least 12 characters long")]
-        [InlineData("User   P@ss w0rd", "Password should not have any whitespaces")]
-        public void Should_not_validate_When(string password, string message)
+        [InlineData("userp@ssw0rd", false, "Password should have at least one uppercase character")]
+        [InlineData("USERP@SSW0RD", false, "Password should have at least one lowercase character")]
+        [InlineData("UserP@ssword", false, "Password should have at least one digit")]
+        [InlineData("UserPassw0rd", false, "Password should have at least one special character")]
+        [InlineData("UserP@sw0rd", false, "Password should be at least 12 characters long")]
+        [InlineData("User   P@ss w0rd", false, "Password should not have any whitespaces")]
+        [InlineData("UserP@ssw0rd", true, null)]
+        public void Should_validate_When(string password, bool valid, string message)
         {
             //Arrange
             var validatioData = new ValidationData(password);
+            var messages = message is not null ? new List<string> { message } : Enumerable.Empty<string>();
 
             //Act
             var result = _passwordValidator.Validate(validatioData);
 
             //Assert
-            result.IsValid.Should().BeFalse();
-            result.Messages.Should().Contain(message);
+            result.IsValid.Should().Be(valid);
+            result.Messages.Should().BeEquivalentTo(messages);
         }
     }
 }
--- src/PasswordValidatorKata/PasswordValidator.cs
+++ src/PasswordValidatorKata/PasswordValidator.cs
@@ -11,12 +11,12 @@ namespace PasswordValidatorKata
     {
         private readonly List<IValidationRule> rules = new List<IValidationRule>()
         {
-            new RegexValidationRule("[A-Z]", "Password should have at least one uppercase character"),
-            new RegexValidationRule("[a-z]", "Password should have at least one lowercase character"),
-            new RegexValidationRule("[0-9]", "Password should have at least one digit"),
-            new RegexValidationRule("[!”#\\$%&'\\(\\)\\*\\+,-\\.\\:;<=>\\?@\\[/\\]^_`\\{\\|\\}~]", "Password should have at least one special character"),
-            new RegexValidationRule(".{12,}", "Password should be at least 12 characters long"),
-            new RegexValidationRule("\\s+$", "Password should not have any whitespaces"),
+            new RegexValidationRule("[A-Z]", true, "Password should have at least one uppercase character"),
+            new RegexValidationRule("[a-z]", true, "Password should have at least one lowercase character"),
+            new RegexValidationRule("[0-9]", true, "Password should have at least one digit"),
+            new RegexValidationRule("[!”#\\$%&'\\(\\)\\*\\+,-\\.\\:;<=>\\?@\\[/\\]^_`\\{\\|\\}~]", true, "Password should have at least one special character"),
+            new RegexValidationRule(".{12,}", true, "Password should be at least 12 characters long"),
+            new RegexValidationRule("(\\s)", false, "Password should not have any whitespaces"),
         };
 
         public ValidationResult Validate(ValidationData validationData)
